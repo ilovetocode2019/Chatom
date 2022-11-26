13a6e1856040e911f533cffb7c9bb5b0a458f85b
ilovetocode2019 <54194229+ilovetocode2019@users.noreply.github.com>
2022-11-25 20:00:21 -0800

    [desktop] Improve app loading UI

--- desktop/main.js
+++ desktop/main.js
@@ -1,15 +1,37 @@
 
-const { app, BrowserWindow, Tray } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
 const path = require('path');
 const isDev = require('electron-is-dev');
+
+const APP_URL = isDev ? 'http://localhost:3000' : 'https://chat.ilovetocode2019.com'
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    autoHideMenuBar: true
+    title: 'Chatom',
+    autoHideMenuBar: true,
+    backgroundColor: '#121212'
   });
-  mainWindow.loadURL(isDev? 'http://localhost:3000' : 'https://chat.ilovetocode2019.com');
+  mainWindow.loadFile('index.html');
+  mainWindow.loadURL(APP_URL);
+
+  mainWindow.webContents.on('did-fail-load', () => {
+    const result = dialog.showMessageBoxSync(mainWindow, {
+      'message': 'Failed to load web client. Make sure you\'re connected to the internet.',
+      'buttons': ['Close', 'Retry'],
+      'title': 'Network Error',
+      'noLink': true
+
+    });
+
+    if (result === 0) {
+      app.quit();
+    } else if (result === 1) {
+      mainWindow.loadURL(APP_URL);
+    }
+  });
+
 }
 
 app.whenReady().then(() => {
